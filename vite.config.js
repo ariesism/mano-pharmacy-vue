5f75ed6e1c1b63516580b4a6b405cd61382be3ee
Edison <ariesism@gmail.com>
2026-07-11 00:30:50 +0800

    feat: add GitHub Actions workflow for deployment and configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGitHubActions ? '/mano-pharmacy-vue/' : '/',
   plugins: [
     vue(),
     tailwindcss()
